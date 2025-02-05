98555a794e18036d0331cd2c415aa343464ae08b
GalahadIII <rphl.levesque@gmail.com>
2025-02-05 14:26:47 +0000

    Ajout des classes pour les races dans le DAO #9

--- data/WorldOfWarcraftDAO.php
+++ data/WorldOfWarcraftDAO.php
@@ -78,6 +78,54 @@ class WorldOfWarcraftDAO {
         CacheManager::set($cacheKey, array_map(fn($spec) => $spec->toArray(), $specialisations));
     
         return $specialisations;
-    }    
+    }
+
+    public static function getRaces(): array {
+        $cacheKey = "races";
+        if ($cachedData = CacheManager::get($cacheKey)) {
+            return array_map(fn($race) => new Race($race), $cachedData);
+        }
+
+        $data = ApiWorldOfWarcraft::faireRequeteApi('playable-race/index');
+        if (!$data || !isset($data['races'])) return [];
+
+        $races = [];
+        foreach ($data['races'] as $raceData) {
+            $raceDetails = self::getRaceDetails($raceData['id']);
+            if ($raceDetails) {
+                $races[] = $raceDetails->toArray();
+            }
+        }
+
+        CacheManager::set($cacheKey, $races);
+        return array_map(fn($race) => new Race($race), $races);
+    }
+
+    public static function getRaceDetails(int $id): ?Race {
+        $cacheKey = "race_$id";
+        if ($cachedData = CacheManager::get($cacheKey)) {
+            return new Race($cachedData);
+        }
+
+        $data = ApiWorldOfWarcraft::faireRequeteApi("playable-race/$id");
+        if (!$data) return null;
+
+        $faction = $data['faction']['name'] ?? 'Inconnu';
+        $classesJouables = array_map(fn($class) => $class['id'], $data['playable_classes'] ?? []);
+
+        $raceData = [
+            'id' => $data['id'],
+            'nom' => $data['name'],
+            'faction' => $faction,
+            'classes_jouables' => $classesJouables
+        ];
+
+        CacheManager::set($cacheKey, $raceData);
+        return new Race($raceData);
+    }
+
+    public static function getRacesParFaction(string $faction): array {
+        return array_filter(self::getRaces(), fn($race) => strtolower($race->faction) === strtolower($faction));
+    }
 }
 
